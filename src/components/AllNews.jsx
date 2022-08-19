a5608496d5eaa62482b57627755b55ec045f82ec
megomax88 <huku.ukuk@yandex.ru>
2022-08-19 13:52:04 +0300

    pokras 2.0

--- src/components/AllNews.jsx
+++ src/components/AllNews.jsx
@@ -6,12 +6,12 @@ export default function AllNews({ authState }) {
   return (
     <div className="mx-auto mt-5" style={{ width: '400px' }}>
       <div style={{ height: '150px' }} />
-      <form className="container bg-secondary rounded-3 py-3 item" align="center">
+      <form className="container zalupa rounded-3 py-3 item" align="center">
         <div className="mb-3">
-          <h2>Полученное значение 1</h2>
+          <h2 className="charnews">Полученное значение 1</h2>
         </div>
         <div className="mb-3">
-          <h2>Полученное значение 2</h2>
+          <h2 className="charnews">Полученное значение 2</h2>
         </div>
         <div className="mx-auto mt-5">
           <h2>И.т.д...</h2>
